f1677e898b44df29324d15d6abd81b673d8af7bf
persiangulf <ccm.blogfa@gmail.com>
2021-05-02 13:02:25 +0430

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to cache matrix inversion.
+##
+## The functions in this module provide a convenient interface
+## to access and cache the inverse of a matrix.  Example usage:
+##
+##   # Create an object to store the matrix and cache its inverse
+##   cacheableMatrix = makeCacheMatrix( rbind(c(10, 20), c(30, 40)) )
+##
+##   # The first call to cacheSolve will calculate the inverse,
+##   inverse1 = cacheSolve(cacheableMatrix)
+##
+##   # Further calls to cacheSolve will be significantly faster, since
+##   # they return the value cached during the previous call.
+##   inverse2 = cacheSolve(cacheableMatrix)
 
-## Write a short comment describing this function
+
+## makeCacheMatrix -- creates an object to hold a matrix and (eventually)
+## its cached inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedinverse <- NULL
+  set <- function (newx) {
+    x <<- newx
+    cachedinverse <<- NULL
+  }
+  get <- function() x
+  setcachedinverse <- function(inverse) cachedinverse <<- inverse
+  getcachedinverse <- function() cachedinverse
+  
+  list(set = set, get = get, setcachedinverse = setcachedinverse, getcachedinverse = getcachedinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve -- returns the inverse of the matrix stored in a
+## 'makeCacheMatrix' structure.  Further calls to cacheSolve() for
+## the same 'makeCacheMatrix' structure will return a cached inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cached <- x$getcachedinverse()
+  if (! is.null(cached)) {
+    return(cached)
+  }
+  m <- x$get()
+  cached <- solve(m)
+  x$setcachedinverse(cached)
+  cached
 }
